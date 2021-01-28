2a5e46a0f4735fe1f6a1c67173821dabb35cf3ba
emersondantas <emerson.ruan@dce.ufpb.br>
2021-01-28 20:41:03 -0300

    (#5) - Adicionado projeto inicial

---
+++ src/main/java/org/ayty/hatcher/api/HatcherApiApplication.java
@@ -0,0 +1,13 @@
+package org.ayty.hatcher.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatcherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatcherApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ayty/hatcher/api/HatcherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ayty.hatcher.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatcherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
