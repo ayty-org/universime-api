50d5a1e5d7eb73b38b91c1041da7c0b48c976b39
Carlos Eduardo <carlos.germano@phoebustecnologia.com.br>
2021-02-11 14:14:53 -0300

    Fix profile dev

--- src/main/resources/db/migration/schema/V20210202162941__create_project_table.sql
+++ src/main/resources/db/migration/schema/V20210202162941__create_project_table.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS project;
 
-CREATE TABLE project (
+CREATE TABLE Project (
     id INT NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    logo TEXT,
+    logo TEXT NOT NULL,
     start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
+    end_date DATE NOT NULL
 );
--- src/test/java/org/ayty/hatcher/api/v1/project/service/UpdateProjectServiceImplTest.java
+++ src/test/java/org/ayty/hatcher/api/v1/project/service/UpdateProjectServiceImplTest.java
@@ -3,41 +3,53 @@ package org.ayty.hatcher.api.v1.project.service;
 import org.ayty.hatcher.api.v1.project.builder.ProjectBuilder;
 import org.ayty.hatcher.api.v1.project.model.Project;
 import org.ayty.hatcher.api.v1.project.jpa.ProjectRepository;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.text.ParseException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
 @Tag("service")
 @ExtendWith(MockitoExtension.class)
 public class UpdateProjectServiceImplTest {
 
-    @InjectMocks
     private UpdateProjectServiceImpl updateProjectService;
+
+    @InjectMocks
+    private FindProjectByIdServiceImpl findProjectByIdService;
 
     @Mock
     private ProjectRepository projectRepository;
+
+    @BeforeEach
+    public void setUp() {
+        this.updateProjectService = new UpdateProjectServiceImpl(this.findProjectByIdService, this.projectRepository);
+    }
 
     @Test
     @DisplayName("update updates project's data when successful")
     public void update_UpdateProjectData_WhenSuccessful() throws ParseException {
 
         Project project = ProjectBuilder.createProject();
-        projectRepository.save(project);
-        project.setName("Projeto Spring Boot 2");
+
+        when(this.projectRepository.findById(1)).thenReturn(Optional.of(project));
+
+        this.updateProjectService.update(project);
 
         ArgumentCaptor<Project> captor = ArgumentCaptor.forClass(Project.class);
-        BDDMockito.verify(projectRepository).save(captor.capture());
+        verify(projectRepository).save(captor.capture());
         Project projectResult = captor.getValue();
 
-        Assertions.assertEquals(projectResult.getName(), "Projeto Spring Boot 2");
+        assertEquals(projectResult.getName(), "Projeto Spring Boot");
     }
 }
